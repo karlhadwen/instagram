a1d7766e0cc37b0242cab2d85715eb52b99544f3
karlhadwen <karlhadwen@gmail.com>
2021-02-24 22:02:47 +0000

    chore: license and readme

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -3,12 +3,12 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: 'AIzaSyCBpRfjMXI0uQ8obfVsUkGDy5jMzorakUY',
-  authDomain: 'instagram-yt.firebaseapp.com',
-  projectId: 'instagram-yt',
-  storageBucket: 'instagram-yt.appspot.com',
-  messagingSenderId: '763311512694',
-  appId: '1:763311512694:web:fc66f0787342163c791357'
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: ''
 };
 
 const firebase = Firebase.initializeApp(config);
