dde67c83fc7408cbd675b678447e6171a4af6f58
veax <borissowadim@mail.ru>
2021-03-26 21:56:15 +0100

    add comments pagination

--- src/components/post/comments.js
+++ src/components/post/comments.js
@@ -6,14 +6,16 @@ import AddComment from './add-comment';
 
 export default function Comments({ docId, comments: allComments, posted, commentInput }) {
   const [comments, setComments] = useState(allComments);
+  const [commentsSlice, setCommentsSlice] = useState(3);
+
+  const showNextComments = () => {
+    setCommentsSlice(commentsSlice + 3);
+  };
 
   return (
     <>
       <div className="p-4 pt-1 pb-4">
-        {comments.length >= 3 && (
-          <p className="text-sm text-gray-base mb-1 cursor-pointer">View all comments</p>
-        )}
-        {comments.slice(0, 3).map((item) => (
+        {comments.slice(0, commentsSlice).map((item) => (
           <p key={`${item.comment}-${item.displayName}`} className="mb-1">
             <Link to={`/p/${item.displayName}`}>
               <span className="mr-1 font-bold">{item.displayName}</span>
@@ -21,6 +23,20 @@ export default function Comments({ docId, comments: allComments, posted, comment
             <span>{item.comment}</span>
           </p>
         ))}
+        {comments.length >= 3 && commentsSlice < comments.length && (
+          <button
+            className="text-sm text-gray-base mb-1 cursor-pointer focus:outline-none"
+            type="button"
+            onClick={showNextComments}
+            onKeyDown={(event) => {
+              if (event.key === 'Enter') {
+                showNextComments();
+              }
+            }}
+          >
+            View more comments
+          </button>
+        )}
         <p className="text-gray-base uppercase text-xs mt-2">
           {formatDistance(posted, new Date())} ago
         </p>
--- src/components/post/footer.js
+++ src/components/post/footer.js
@@ -2,9 +2,9 @@ import PropTypes from 'prop-types';
 
 export default function Footer({ caption, username }) {
   return (
-    <div className="p-4 pt-2 pb-0">
+    <div className="p-4 pt-2 pb-1">
       <span className="mr-1 font-bold">{username}</span>
-      <span>{caption}</span>
+      <span className="italic">{caption}</span>
     </div>
   );
 }
